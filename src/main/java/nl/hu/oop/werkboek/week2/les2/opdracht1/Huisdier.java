fae4646b346757e9e52d97d6234f5535bf614ff8
Tom <tomkempernl@gmail.com>
2022-02-04 09:10:48 +0100

    Opdracht 4_1 oplossing

--- src/main/java/nl/hu/oop/werkboek/week2/les2/opdracht1/Huisdier.java
+++ src/main/java/nl/hu/oop/werkboek/week2/les2/opdracht1/Huisdier.java
@@ -4,18 +4,41 @@ public class Huisdier {
     private String naam;
     private String ras;
     private double gewicht;
+    private Eigenaar baasje;
 
     public Huisdier(String nm, String r) {
         naam = nm;
         ras = r;
         gewicht = 0.0;
     }
-    public String getNaam() { return naam; }
-    public String getRas() { return ras; }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public String getRas() {
+        return ras;
+    }
+
     public String toString() {
-        return naam + ", de " + ras + ", weegt " + gewicht + " kg.";
+        String regel1 = naam + ", de " + ras + ", weegt " + gewicht + " kg.";
+        String regel2;
+
+        if (this.baasje == null) {
+            regel2 = "En de eigenaar is: null";
+        } else {
+            regel2 = "En de eigenaar is: " + this.baasje.toString();
+        }
+
+        return regel1 + "\n" + regel2;
     }
 
     public void setBaasje(Eigenaar e1) {
+        this.baasje = e1;
+    }
+
+    //Technisch gezien niet nodig, maar het is een beetje ongebruikelijk om een setter zonder getter te hebben
+    public Eigenaar getBaasje() {
+        return this.baasje;
     }
 }
