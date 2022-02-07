d7cc6b8c180b050e82d1de29c0527912575da348
Tom <tom.kemper@hu.nl>
2022-02-07 21:36:47 +0100

    Compileren leren door decompileren

---
+++ src/main/java/nl/hu/oop/demos/Decompilen.java
@@ -0,0 +1,10 @@
+package nl.hu.oop.demos;
+
+public class Decompilen {
+    public static void main(String[] args) {
+        String message = "Hello World";
+        for (int i = 0; i < 10; i++) {
+            System.out.println(message);
+        }
+    }
+}
---
+++ src/main/java/nl/hu/oop/demos/Decompilen2.java
@@ -0,0 +1,12 @@
+package nl.hu.oop.demos;
+
+public class Decompilen2 {
+    public static void main(String[] args) {
+        String message = "Hello World";
+        int i = 0;
+        while (i < 10) {
+            System.out.println(message);
+            i++;
+        }
+    }
+}
